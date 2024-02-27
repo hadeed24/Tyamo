6c1221c98d65eb975a484efc70c1fff5001d84f4
hadeed <abdulhadeedhamirani@gmail.com>
2024-02-27 17:51:35 +0500

    edits

--- lib/Views/features/Album/album_post.dart
+++ lib/Views/features/Album/album_post.dart
@@ -1,9 +1,15 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:expandable_text/expandable_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_balloon_slider/flutter_balloon_slider.dart';
+import 'package:focused_menu/focused_menu.dart';
+import 'package:focused_menu/modals.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:getwidget/getwidget.dart';
 import 'dart:ui' as prefix0;
 
 import 'package:google_fonts/google_fonts.dart';
+import 'package:list_tile_more_customizable/list_tile_more_customizable.dart';
 
 class AlbumPost extends StatelessWidget {
   const AlbumPost({super.key});
@@ -122,25 +128,205 @@ class AlbumPost extends StatelessWidget {
                   ),
                 ),
               ),
-              const SizedBox(
-                height: 15,
-              ),
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Text(
                           "Caption: ",
                           style: GoogleFonts.nunito(
-                            fontSize: 15,
+                              fontSize: 15,
                               fontWeight: FontWeight.w900,
-                              color: Color(0xFF00043D)),
-                        ), SizedBox(width: 10,),
-                       
+                              color: const Color(0xFF00043D)),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            awesomedialog(context);
+                          },
+                          child: const Icon(
+                            Icons.edit,
+                            color: Color(0xFF00043D),
+                          ),
+                        )
                       ],
                     ),
+                    FocusedMenuHolder(
+                      onPressed: () {},
+                      menuItems: [
+                        FocusedMenuItem(
+                            title: Text(
+                              "Copy",
+                              style: GoogleFonts.nunito(
+                                  color: const Color(0xFF00043D),
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            onPressed: () {},
+                            trailingIcon: const Icon(FontAwesomeIcons.copy,
+                                color: Color(0xFF00043D))),
+                        FocusedMenuItem(
+                          title: Text(
+                            "Edit",
+                            style: GoogleFonts.nunito(
+                                color: const Color(0xFF00043D),
+                                fontWeight: FontWeight.bold),
+                          ),
+                          onPressed: () {
+                            awesomedialog(context);
+                          },
+                          trailingIcon: const Icon(
+                            FontAwesomeIcons.edit,
+                            color: Color(0xFF00043D),
+                          ),
+                        ),
+                        FocusedMenuItem(
+                          title: Text(
+                            "Delete",
+                            style: GoogleFonts.nunito(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          onPressed: () {
+                            awesomedialog(context);
+                          },
+                          trailingIcon: const Icon(
+                            FontAwesomeIcons.trash,
+                            color: Colors.white,
+                          ),
+                          backgroundColor: Color(0xFF00043D),
+                        )
+                      ],
+                      menuWidth: MediaQuery.of(context).size.width / 2,
+                      blurSize: 2.0,
+                      menuItemExtent: 45,
+                      menuBoxDecoration: const BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(15),
+                        ),
+                      ),
+                      duration: const Duration(milliseconds: 100),
+                      animateMenuItems: true,
+                      blurBackgroundColor: Colors.black54,
+                      menuOffset: 10,
+                      bottomOffsetHeight: 80,
+                      child: ExpandableText(
+                        "Place caption",
+                        expandText: 'show more',
+                        collapseText: 'show less',
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.nunito(
+                          fontWeight: FontWeight.w400,
+                          color: Colors.black,
+                        ),
+                        maxLines: 2,
+                        linkColor: Colors.amberAccent,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Text(
+                      "Comments",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.nunito(
+                        fontWeight: FontWeight.w900,
+                        fontSize: 18,
+                        color: const Color(0xFF00043D),
+                      ),
+                    ),
+                    ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: 1,
+                      itemBuilder: (context, index) {
+                        return FocusedMenuHolder(
+                          menuItems: [
+                            FocusedMenuItem(
+                                title: Text(
+                                  "Copy",
+                                  style: GoogleFonts.nunito(
+                                      color: const Color(0xFF00043D),
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                onPressed: () {},
+                                trailingIcon: const Icon(FontAwesomeIcons.copy,
+                                    color: Color(0xFF00043D))),
+                            FocusedMenuItem(
+                              title: Text(
+                                "Delete",
+                                style: GoogleFonts.nunito(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              onPressed: () {},
+                              trailingIcon: const Icon(
+                                FontAwesomeIcons.trash,
+                                color: Colors.white,
+                              ),
+                              backgroundColor: Colors.redAccent,
+                            )
+                          ],
+                          menuWidth: MediaQuery.of(context).size.width / 2,
+                          blurSize: 2.0,
+                          menuItemExtent: 45,
+                          menuBoxDecoration: const BoxDecoration(
+                            color: Colors.grey,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(15),
+                            ),
+                          ),
+                          duration: const Duration(milliseconds: 100),
+                          animateMenuItems: true,
+                          blurBackgroundColor: Colors.black54,
+                          menuOffset: 10,
+                          bottomOffsetHeight: 80,
+                          onPressed: () {},
+                          child: ListTileMoreCustomizable(
+                            title: Text(
+                              "@faizan",
+                              style: GoogleFonts.nunito(
+                                fontSize: 15,
+                                color: const Color(0xFF00043D),
+                              ),
+                            ),
+                            leading: GFAvatar(
+                              size: 25,
+                              child: Image.network(
+                                "https://images.pexels.com/photos/11650554/pexels-photo-11650554.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                              ),
+                            ),
+                            minLeadingWidth: 60,
+                            horizontalTitleGap: 0,
+                            minVerticalPadding: 10,
+                            contentPadding: const EdgeInsets.all(0),
+                            isThreeLine: false,
+                            subtitle: Align(
+                              alignment: Alignment.centerLeft,
+                              child: Text(
+                                "SSSSSSSSSSSSSSSSSSSSSS",
+                                textAlign: TextAlign.justify,
+                                style: GoogleFonts.nunito(
+                                    fontSize: 16, fontWeight: FontWeight.w400),
+                              ),
+                            ),
+                            trailing: Text(
+                              "Just now",
+                              style: GoogleFonts.nunito(
+                                fontSize: 11,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            onTap: null,
+                            onLongPress: null,
+                          ),
+                        );
+                      },
+                    )
                   ],
                 ),
               )
@@ -149,5 +335,65 @@ class AlbumPost extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void awesomedialog(BuildContext context) {
+    AwesomeDialog(
+      context: context,
+      animType: AnimType.scale,
+      dialogType: DialogType.noHeader,
+      title: "Edit Caption",
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Text(
+                "Edit Caption",
+                style: GoogleFonts.nunito(
+                    color: const Color(0xFF00043D),
+                    fontSize: 20,
+                    fontWeight: FontWeight.w900),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              TextField(
+                controller: null,
+                decoration: InputDecoration(
+                  labelText: "Enter new caption",
+                  labelStyle: GoogleFonts.nunito(color: Colors.black),
+                  fillColor: Colors.black,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: const BorderSide(
+                      color: Color(0xFF00043D),
+                    ),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: const BorderSide(color: Colors.black),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: const BorderSide(
+                      color: Color(0xFF00043D),
+                    ),
+                  ),
+                ),
+                keyboardType: TextInputType.multiline,
+                minLines: 1,
+                maxLines: null,
+              ),
+            ],
+          ),
+        ),
+      ),
+      dismissOnBackKeyPress: true,
+      isDense: false,
+      btnOkOnPress: () {},
+      btnOkText: "Confirm caption",
+      titleTextStyle: GoogleFonts.nunito(),
+    ).show();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
           theme: ThemeData(),
           debugShowCheckedModeBanner: false,
           title: 'Tyamo',
-          home: const AlbumPost());
+          home:  AlbumPost());
     });
   }
 }
